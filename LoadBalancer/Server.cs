0d448a35f9e41ccada4e3f922e0a676bfe7be52c
Sheersendu <shibrata15@gmail.com>
2025-07-06 15:44:09 +0530

    Thread safe implementation of Load balancer

---
+++ LoadBalancer/Server.cs
@@ -0,0 +1,30 @@
+﻿namespace LoadBalancer;
+
+public class Server
+{
+	public readonly Guid Id = Guid.NewGuid();
+	public readonly string Name;
+	public volatile bool Status = true; //volatile to ensure visibility across threads
+	public int _totalConnections = 0;
+
+	public Server(string name)
+	{
+		Name = name;
+	}
+
+	public void SetStatus(bool status)
+	{
+		Status = status;
+	}
+
+	public void AddConnection()
+	{
+		Interlocked.Increment(ref _totalConnections);
+	}
+
+	public void Process()
+	{
+		Console.WriteLine($"Server {Name} is processing request!");
+		AddConnection();
+	}
+}
