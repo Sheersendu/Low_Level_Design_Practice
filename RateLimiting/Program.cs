170d95f094c4a88e758b6c4d7dc1c13648875bf4
Sheersendu <shibrata15@gmail.com>
2025-07-10 19:52:34 +0530

    Implemented Rate limiter

---
+++ RateLimiting/Program.cs
@@ -0,0 +1,9 @@
+﻿using RateLimiting;
+
+RateLimiter rateLimiter = new ();
+for (int request = 0; request < 8; request++)
+{
+	if (request == 3)
+		Thread.Sleep(TimeSpan.FromMinutes(6));
+	rateLimiter.ProcessRequest(1);
+}
---
+++ RateLimiting/Token.cs
@@ -0,0 +1,26 @@
+﻿namespace RateLimiting;
+
+public class Token
+{
+	private DateTime _lastRefilledTimeStamp = DateTime.UtcNow;
+	public volatile int TokenCount = 2;
+	private readonly object _lockObject = new();
+
+	public DateTime LastRefilledTimeStamp
+	{
+		get
+		{
+			lock (_lockObject)
+			{
+				return _lastRefilledTimeStamp;
+			}
+		}
+		set
+		{
+			lock (_lockObject)
+			{
+				_lastRefilledTimeStamp = value;
+			}
+		}
+	}
+}
