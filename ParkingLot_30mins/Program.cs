ed3e8beb977eea2d0b6a85fd804ba480733ceeb5
Sheersendu <shibrata15@gmail.com>
2025-08-30 22:33:10 +0530

    Parking Lot LLD with UML

---
+++ ParkingLot_30mins/Program.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections.Concurrent;
+
+enum SpotType { Small, Medium, Large }
+class ParkingSpot
+{
+	private Guid Id;
+	private string Number;
+	private SpotType Type;
+	private Vehicle? vehicle;
+	private bool Available = true;
+	private object spotLock = new ();
+
+	public void Reserve(Vehicle currentVehicle)
+	{
+		lock (spotLock)
+		{
+			if (Available)
+			{
+				Available = false;
+				vehicle = currentVehicle;
+			}
+			else
+			{
+				Console.WriteLine("Seat already reserved!");
+			}
+		}
+	}
+
+	public void UnReserve()
+	{
+		lock (spotLock)
+		{
+			Available = true;
+			vehicle = null;
+		}
+	}
+
+	public bool IsAvailable()
+	{
+		lock (spotLock)
+		{
+			return Available;
+		}
+	}
+
+	public SpotType GetSpotType()
+	{
+		return Type;
+	}
+	
+}
+
+class Floor
+{
+	private List<ParkingSpot> AllSpots = new();
+	private object floorLock = new();
+
+	public List<ParkingSpot> GetAvailableSpots(SpotType type)
+	{
+		lock (floorLock)
+		{
+			return AllSpots.Where(spot => spot.IsAvailable() && spot.GetType().Equals(type)).ToList();
+		}
+	}
+}
+
+enum VehicleType { Car, Bike, Truck }
+
+abstract class Vehicle
+{
+	private string LicenceNumber;
+	private VehicleType Type;
+
+	public VehicleType GetVehicleType()
+	{
+		return Type;
+	}
+}
+
+class Car : Vehicle{}
+class Bike : Vehicle{}
+class Truck : Vehicle{}
+
+class Ticket(Vehicle vehicle, ParkingSpot spot)
+{
+	private Guid Id = Guid.NewGuid();
+	private Vehicle vehicle = vehicle;
+	private ParkingSpot spot = spot;
+	private DateTime Entry = DateTime.UtcNow;
+	private DateTime Exit;
+
+	public int CalculateDuration()
+	{
+		return (int)(Exit - Entry).TotalMinutes;
+	}
+
+	public VehicleType GetVehicleType()
+	{
+		return vehicle.GetVehicleType();
+	}
+
+	public ParkingSpot GetSpot()
+	{
+		return spot;
+	}
+
+	public void SetExitTime()
+	{
+		Exit = DateTime.UtcNow;
+	}
+}
+
+interface IFareStrategy
+{
+	double GetFare(Ticket ticket);
+}
+
+class BaseFareStrategy : IFareStrategy
+{
+	private ConcurrentDictionary<VehicleType, double> fare = new();
+
+	public BaseFareStrategy()
+	{
+		fare.TryAdd(VehicleType.Car, 2.500);
+		fare.TryAdd(VehicleType.Bike, 2.050);
+		fare.TryAdd(VehicleType.Truck, 5.099);
+	}
+	
+	public double GetFare(Ticket ticket)
+	{
+		return fare.GetValueOrDefault(ticket.GetVehicleType(), 1)*ticket.CalculateDuration();
+	}
+}
+
+class ParkingLot
+{
+	private List<Floor> AllFloors;
+	private IFareStrategy Strategy = new BaseFareStrategy();
+
+	public List<ParkingSpot> FindAvailableSpots(SpotType type)
+	{
+		List<ParkingSpot> AllSpots = new();
+
+		foreach (Floor floor in AllFloors)
+		{
+			AllSpots.AddRange(floor.GetAvailableSpots(type));
+		}
+
+		return AllSpots;
+	}
+
+	public Ticket Park(ParkingSpot spot, Vehicle vehicle)
+	{
+		spot.Reserve(vehicle);
+		Ticket newTicket = new(vehicle, spot);
+		return newTicket;
+	}
+
+	public double UnPark(Ticket ticket)
+	{
+		ParkingSpot spot = ticket.GetSpot();
+		spot.UnReserve();
+		ticket.SetExitTime();
+		return Strategy.GetFare(ticket);
+	}
+}
+
---
+++ Uber_Strategy/Program.cs
@@ -0,0 +1,201 @@
+﻿using System.Collections.Concurrent;
+using System.Threading.Channels;
+
+class BaseClass
+{
+	public Guid Id { get; init; } = Guid.NewGuid();
+	public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+}
+
+public class Location(double longitude, double latitude )
+{
+	public double Latitude { get; set; } = latitude;
+	public double Longitude { get; set; } = longitude;
+}
+
+class Rider(string name, string phoneNumber) : BaseClass
+{
+	public string Name { get; set; } = name;
+	public string PhoneNumber { get; set; } = phoneNumber;
+	public Location CurrentLocation { get; set; }
+}
+
+enum DriverStatus
+{
+	Available, Offline, InRide
+}
+
+
+class Driver(string name, string licence) : BaseClass
+{
+	public string Name { get; set; } = name;
+	public string Licence { get; set; } = licence;
+	public DriverStatus Status { get; set; } = DriverStatus.Available;
+	private Object Lock = new();
+
+	public void UpdateStatus(DriverStatus newStatus)
+	{
+		lock (Lock)
+		{
+			Status = newStatus;
+		}
+	}
+}
+
+enum RideStatus
+{
+	Scheduled, InProgress, Cancelled
+}
+
+class Ride(Rider rider, Location pickup, Location drop) : BaseClass
+{
+	public Rider CurrentRider { get; set; } = rider;
+	public Driver? CurrentDriver { get; set; }
+	public Location Pickup { get; set; } = pickup;
+	public Location Drop { get; set; } = drop;
+	public double? Fare { get; set; }
+	public RideStatus Status { get; set; } = RideStatus.Scheduled;
+	private Object Lock = new();
+
+	public void UpdateStatus(RideStatus newStatus)
+	{
+		lock (Lock)
+		{
+			Status = newStatus;
+		}
+	}
+
+	public void UpdateDriver(Driver driver)
+	{
+		lock (Lock)
+		{
+			CurrentDriver = driver;
+		}
+	}
+}
+
+interface IFareStrategy
+{
+	double CalculateFare(Ride ride);
+}
+
+class BaseStrategy : IFareStrategy
+{
+	public double CalculateFare(Ride ride)
+	{
+		return 100;
+	}
+}
+
+class SurgeStrategy : IFareStrategy
+{
+	public double CalculateFare(Ride ride)
+	{
+		return 150;
+	}
+}
+
+
+class RideBooking
+{
+	private static RideBooking Instance;
+	private static Object rideBookingInstance = new();
+	private static Object rideLock = new();
+	private ConcurrentQueue<Ride> ScheduledRides = new();
+	private ConcurrentDictionary<Guid, Ride> UserRides = new();
+	private ConcurrentBag<Driver> DriverList = new();
+	private IFareStrategy strategy = new BaseStrategy();
+	
+	private RideBooking(){}
+
+	public static RideBooking GetInstance()
+	{
+		if (Instance is null)
+		{
+			lock (rideBookingInstance)
+			{
+				if (Instance is null)
+				{
+					Instance = new RideBooking();
+				}
+			}
+		}
+
+		return Instance;
+	}
+
+	public void SetStrategy(IFareStrategy newStrategy)
+	{
+		lock (rideBookingInstance)
+		{
+			strategy = newStrategy;
+		}
+	}
+
+	public void ScheduleRide(Rider rider, Location pickup, Location drop)
+	{
+		Ride newRide = new(rider, pickup, drop);
+		ScheduledRides.Enqueue(newRide);
+		UserRides.TryAdd(rider.Id, newRide);
+		NotifyDrivers(newRide);
+	}
+
+	public void AddDriver(string name, string licence)
+	{
+		Driver newDriver = new(name, licence);
+		DriverList.Add(newDriver);
+	}
+
+	private void NotifyDrivers(Ride ride)
+	{
+		foreach(Driver driver in DriverList)
+		{
+			if (driver.Status.Equals(DriverStatus.Available))
+			{
+				Console.WriteLine($"Notified Driver {driver.Name}");
+				AcceptRide(ride, driver);
+				break;
+			}
+		}
+	}
+
+	private void AcceptRide(Ride ride, Driver driver)
+	{
+		lock (rideLock)
+		{
+			ride.UpdateStatus(RideStatus.InProgress);
+			ride.UpdateDriver(driver);
+			driver.UpdateStatus(DriverStatus.InRide);
+		}
+	}
+
+	private void CancelRide(Ride ride)
+	{
+		lock (rideLock)
+		{
+			if (!(ride.CurrentDriver is null))
+			{
+				Driver driver = ride.CurrentDriver;
+				driver.UpdateStatus(DriverStatus.Available);
+				ride.CurrentDriver = null;
+			}
+			ride.UpdateStatus(RideStatus.Cancelled);
+		}
+	}
+	
+}
+
+
+
+class UberDemo
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Book your ride now!");
+		RideBooking booking = RideBooking.GetInstance();
+		Rider rider = new("A", "1");
+		booking.AddDriver("B", "L1");
+		booking.ScheduleRide(rider, new Location(12.1, 13.5), new Location(23.5, 21.6));
+		
+	}
+}
